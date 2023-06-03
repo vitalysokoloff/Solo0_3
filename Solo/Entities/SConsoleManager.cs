959de92ca306e839783a6bce385eebf69b1b0015
Vitaly Sokoloff <vitaliysokoloff@gmail.com>
2023-06-03 14:46:57 +0700

    Стилизация консоли

--- Solo/Entities/SConsoleManager.cs
+++ Solo/Entities/SConsoleManager.cs
@@ -10,22 +10,29 @@ namespace Solo
 {
     public class SConsoleManager : IEntity
     {
-        public Heap Configs{ get; set;}
+        public Heap Configs;
+        public Texture2D Texture;
+        public Rectangle SourceRectangle;
+        public Rectangle DrawRectangle;
 
         private GraphicsDeviceManager _graphics;
         private KeysInput _input;
 
         public SConsoleManager(GraphicsDeviceManager graphics, SpriteFont font)
         {
-            Configs = new Heap();
+            Configs = new Heap();            
             _graphics = graphics;
+
+            Texture = Tools.MakeSolidColorTexture(_graphics, new Point(1, 1), new Color(34, 34, 34));
+            SourceRectangle = new Rectangle(0, 0, 1, 1);
+            DrawRectangle = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight / 2);
             _input = new KeysInput();
             _input.Add("console", new Key(Keys.OemTilde));
             _input.Add("unlockConsole", new Key(Keys.F1));
             _input.Add("input", new Key(Keys.Enter));
             SConsole.Font = font;
             SConsole.FontColor = Color.White;
-            SConsole.Position = new Vector2(5, _graphics.PreferredBackBufferHeight / 2);
+            SConsole.Position = new Vector2(15, _graphics.PreferredBackBufferHeight / 2);
             SConsole.Off();
             SConsole.isTextInput = true;
         }
@@ -71,6 +78,11 @@ namespace Solo
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            if (SConsole.GetState())
+            {
+                spriteBatch.Draw(Texture, DrawRectangle, SourceRectangle, Color.White * 0.9f);
+                spriteBatch.DrawString(SConsole.Font, ">", SConsole.Position - new Vector2(10, SConsole.Font.MeasureString(">").Y), Color.White);
+            }
             SConsole.Draw(gameTime, spriteBatch);
         }
     }
